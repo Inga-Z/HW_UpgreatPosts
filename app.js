bf2158e66f0525f678cba6711b0ca3dda964e2d8
Inga-Z <z_inga@mail.ru>
2019-11-21 23:10:37 +0300

    FourthStep

--- app.js
+++ app.js
@@ -5,76 +5,103 @@ let nextId = 1;
 
 const rootEl = document.getElementById('root');
 
-
 const formEl = document.createElement('form');
 formEl.className = 'form-inline';
 
 formEl.innerHTML = `
-    <input type="text" class="form-control mb-2 mr-sm-2" data-id="url">
-    <select type="text" class="form-control mb-2 mr-sm-2" data-id="type">
+    <input type="text" class="form-control" data-id="link">
+    <select class="form-control" data-id="type">
         <option value="regular">Текст</option>
         <option value="image">Изображение</option>
-        <option value="audio">Аудио</option>
         <option value="video">Видео</option>
+        <option value="audio">Аудио</option>
     </select>
-    <button class=btn btn-light mb-2" data-action="add">OK</button>
+    <button class="btn btn-light" data-action="add">OK</button>
 `;
+const linkEl = formEl.querySelector('[data-id=link]');
+const typeEl = formEl.querySelector('[data-id=type]');
 
-const urlEl = formEl.querySelector('[data-id=url]');
-
-const textEl = formEl.querySelector('[data-id=text]');
 formEl.addEventListener('submit', (event) => {
     event.preventDefault();
-
-    const url = urlEl.value;
+    const value = linkEl.value;
+    const type = typeEl.value;
+   
     
-
-    const post = {
+    posts.push({
         id: nextId++,
-        url,
+        value,
+        type,
         likes: 0,
-    };
-    posts.push(post);
-    rebuildList(postsEl, posts);
+    },);
 
-    urlEl.value = '';
+    linkEl.value = '';
+    linkEl.focus();
+        posts.sort((a,b) => {
+            return b.likes - a.likes
+        })
     
-    urlEl.focus();
-});
+    rebuildList(postsEl, posts);
+    });
 
 rootEl.appendChild(formEl);
 
-const postsEl = document.createElement('ul');
+const postsEl = document.createElement('div');
 postsEl.className = 'list-group';
 
 rootEl.appendChild(postsEl);
 
 function rebuildList(containerEl, items) {
+
     for(const child of Array.from(containerEl.children)) {
         containerEl.removeChild(child);
     }
     for (const item of items) {
-        const el = document.createElement('li');
-        el.className = 'list-group-item';
-        el.dataset.id = `post-${item.id}`;
-        el.innerHTML = `
-            <img src="${item.url}" class="rounded" width="50" height="50">
-        
-            <span class="badge badge-secondary">${item.likes}</span>
-            <button type="button" class="btn btn-primary btn-sm" data-action="like">like</button>
-            <button type="button" class="btn btn-primary btn-sm" data-action="dislike">dislike</button>
-        `;
-        el.querySelector('[data-action=like]').addEventListener('click', (event) => {
-            item.likes++;
-            rebuildList(containerEl, items);
-         
-        });
-        el.querySelector('[data-action=dislike]').addEventListener('click', (event) => {
-            item.likes--;
-            rebuildList(containerEl, items);
-        });
-        containerEl.insertBefore(el, containerEl.firstElementChild);
-    }
-}
+        const postsEl = document.createElement('div');
+        postsEl.className = 'list-group-item';
+        postsEl.dataset.id = `post-${item.id}`;
+       
+        if (item.type == 'image') {
+            postsEl.innerHTML = `
+                <img src="${item.value}" class="rounded" width="50" height="50">
+                <button class="btn btn-primary" data-action="like"> ❤ ${item.likes}</button>
+                <button class="btn btn-primary" data-action="dislike">dislike</button>
+            `;
+        }
+        else if (item.type == 'video') {
+            postsEl.innerHTML = `
+                <video src = "${item.value}" class = "embed-responsive-item" controls>
+                <button class="btn btn-primary" data-action="like"> ❤ ${item.likes}</button>
+                <button class="btn btn-primary" data-action="dislike">dislike</button>
+             `;
+         }
+        else if (item.type == 'audio') {
+            postsEl.innerHTML = `
+                <audio src = "${item.value}" class = "embed-responsive-item" controls>
+                <button class="btn btn-primary" data-action="like"> ❤ ${item.likes}</button>
+                <button class="btn btn-primary" data-action="dislike">dislike</button>
+             `;
+        }
 
+
+        postsEl.querySelector('[data-action=like]').addEventListener('click', (event) => {
+            item.likes++;
+            items.sort((a, b) => {
+                return b.likes - a.likes;
+            })
+            rebuildList(containerEl, items);
+        });
+        console.dir(postsEl);
+
+        postsEl.querySelector('[data-action=dislike]').addEventListener('click', (event) => {
+            item.likes--;
+            items.sort((a, b) => {
+                return b.likes - a.likes;
+            })
+            rebuildList(containerEl, items);
+        });
+
+    
+        containerEl.insertBefore(postsEl, containerEl.nextSibling);
+    }
+};
 
